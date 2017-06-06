9749b68a24f1faae606ecfe3f8000f02ec31d995
nandhinir31 <nandhinir@acetcbe.edu.in>
2017-06-06 13:48:08 +0530

    Create array2.c

---
+++ array2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int arr[5];
+clrscr();
+	int i,j,n=5;
+	int ind,ele; 	
+	for(i=0; i<n; i++)
+	{
+		printf("Enter element %d: ",i+1);
+		scanf("%d",&arr[i]);
+	}
+	
+	printf("Array elements are: ");
+	for(i=0; i<n; i++)
+		printf("%d ",arr[i]);
+	
+	printf("\n");
+	ind=-1;
+		for(i=0; i<n; i++)
+	{
+		for(j=i+1; j<n; j++)
+		{
+			if(arr[i]==arr[j])
+			{
+				ele=arr[j];
+				ind=j;
+				break;
+			}				
+		}
+	}
+	if(ind!=-1)
+		printf("%d repeated @ %d index\n",ele,ind);
+	else
+		printf("There is no repeated element\n");
+getch();
+
+	return 0;
+}
